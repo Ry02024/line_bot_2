d6bf4eedf60314902f40362209a66a1a46a40d8d
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2025-01-26 08:50:02 +0900

    Update main.py

--- src/routes/main.py
+++ src/routes/main.py
@@ -1,10 +1,6 @@
 import random
 import sys
 import os
-
-# プロジェクトルートをパスに追加
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from services.config import Config
 from services.gemini import Gemini
 from services.line_bot import LineBot
