29ac5acf42e4a0e61e9a4f573ddd8ccc420e98b5
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2025-01-26 10:00:09 +0900

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="line_bot_2",
     version="0.1",
-    packages=find_packages(),  # パッケージを自動検出
-    package_dir={"": "."},  # src 内のモジュールを検出
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
